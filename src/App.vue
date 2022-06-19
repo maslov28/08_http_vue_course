d162a2553006f0c0610d5db3b8c87bfa549f0156
Artem <artem.fatefulchoice@mail.ru>
2022-06-19 19:48:48 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <form class="card" @submit.prevent="createPerson">
+      <h2>Работа с базой данных</h2>
+
+      <div class="form-control">
+        <label for="name">Введите имя</label>
+        <input type="text" id="name" v-model.trim="name">
+      </div>
+
+      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
+    </form>
+
+    <app-people-list 
+      :people="people"
+      @load="loadPeople"
+      @remove="removePeople"
+    ></app-people-list>
+  </div>
+  
+
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppPeopleList from './AppPeopleList.vue'
+import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      name: '',
+      people: []
+    }
+  },
+  mounted() {
+    this.loadPeople()
+  },
+  methods: {
+    async createPerson() {
+      const response = await fetch('https://vue-with-http-f9ba5-default-rtdb.firebaseio.com/people.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: this.name
+        })
+      })
+
+      const firebaseData = await response.json()
+
+      this.people.push({
+        firstName: this.name,
+        id: firebaseData.name
+      })
+
+      this.name = ''
+    },
+    async loadPeople() {
+      const {data} = await axios.get('https://vue-with-http-f9ba5-default-rtdb.firebaseio.com/people.json')
+      this.people = Object.keys(data).map(key => {
+        return {
+          id: key,
+          ...data[key]
+        }
+      })
+    },
+    removePeople() {
+      
+    }
+  },
+  components: {AppPeopleList}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
